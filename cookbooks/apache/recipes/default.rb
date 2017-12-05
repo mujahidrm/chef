5f9b7513bd639189afe9c11e5739246e4a978e4d
Mujahid Rasheed <terkukaiku@gmail.com>
2017-12-05 22:45:45 +0000

    Added cookbook apache and created a recipe named websites in it

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+package 'apache web server' do
+	package_name 'httpd'
+end
+
+service 'apache web service' do
+	service_name 'httpd'
+end
---
+++ cookbooks/apache/recipes/websites.rb
@@ -0,0 +1,4 @@
+file 'default httpd turn page' do
+	path '/var/www/html/index.html'
+        content ' This is my index.html file'
+end
